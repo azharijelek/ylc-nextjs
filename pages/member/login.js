78730b4d761b31a8d67dbd0a76ea31ad92cc5922
Azhari Subroto <juarathemes@gmail.com>
2020-10-12 15:51:12 +0700

    login performance

--- pages/member/login.js
+++ pages/member/login.js
@@ -4,8 +4,6 @@ import Box from '@material-ui/core/Box'
 import Head from 'next/head'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
-import { useSelector, useDispatch } from 'react-redux'
-import { incrementCount, decrementCount, resetCount } from '@/store'
 import fetchJson from '@/lib/fetchJson'
 import useUser from '@/lib/useUser'
 
