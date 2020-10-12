717ed9e8b31d7b754b1cd2592aefc959ba6cc7c1
Azhari Subroto <juarathemes@gmail.com>
2020-10-12 10:16:26 +0700

    redux

--- pages/member/login.js
+++ pages/member/login.js
@@ -1,92 +1,91 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import actions from '../../redux/actions';
-import initialize from '../../utils/initialize';
-/* Components */
+import React from "react";
 import Container from '@material-ui/core/Container'
 import Box from '@material-ui/core/Box'
 import Head from 'next/head'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button'
+import UserServices from '@/services/UserServices'
 
-class Signin extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
+export default function Login() {
+    
+    const [values, setValues] = React.useState({
+        username: '',
+        password: '',
+    });
+    
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
     };
-  }
 
-  static getInitialProps(ctx) {
-    initialize(ctx);
-  }
+    
+    /**
+     * Handle Login
+     * @param {*} e 
+     */
+    const onSubmit = async () => {
+        const payload = {
+            username: values.username,
+            password: values.password
+        }
+        try {
+            const res = await UserServices.login(payload);
 
-  handleSubmit(e) {
-    e.preventDefault();
-    this.props.authenticate(
-      { email: this.state.email, password: this.state.password },
-      'signin'
-    );
-  }
+            if( res.status == 200 ) {
+                const data = await res.data
+                console.log(data);
+                localStorage.setItem('token', );
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
 
-  render() {
     return (
         <>
             <Head>
                 <title>Member Login - Your Life Choices</title>
             </Head>
             <Container maxWidth="lg">
+
                 <main>
+                    <pre>{JSON.stringify(values, null, 2)}</pre>
                     <h1 className="text-center">Login to YourLifeChoices Account</h1>
-                    <form
-                    onSubmit={this.handleSubmit.bind(this)}
-                    className="container"
-                    style={{ width: '540px' }}
-                    >
-                    
-                        <Box my={3}>
-                            <TextField
-                            type="text"
-                            label="Email/Username"
-                            variant="outlined"
-                            color="primary"
-                            onChange={e => this.setState({ email: e.target.value })}
-                            value={this.state.email}
-                            fullWidth
-                            />
-                        </Box>
+                    <Box my={3}>
+                        <TextField
+                        type="text"
+                        label="Email/Username"
+                        variant="outlined"
+                        color="primary"
+                        onChange={handleChange('username')}
+                        value={values.username}
+                        fullWidth
+                        />
+                    </Box>
 
-                        <Box my={3}>
-                            <TextField
-                            type="password"
-                            label="Password"
-                            variant="outlined"
-                            color="primary"
-                            value={this.state.password}
-                            onChange={e => this.setState({ password: e.target.value })}
-                            fullWidth
-                            />
-                        </Box>
+                    <Box my={3}>
+                        <TextField
+                        type="password"
+                        label="Password"
+                        variant="outlined"
+                        color="primary"
+                        onChange={handleChange('password')}
+                        value={values.password}
+                        fullWidth
+                        />
+                    </Box>
 
-                        <Button
-                        color="primary" 
-                        disableElevation={true}
-                        variant="contained"
-                        size="large"
-                        type="submit"
-                        fullWidth>
-                            Sign In
-                        </Button>
-                    </form>
+                    <Button
+                    color="primary" 
+                    disableElevation={true}
+                    variant="contained"
+                    size="large"
+                    type="submit"
+                    onClick={() => { onSubmit() }}
+                    fullWidth>
+                        Sign In
+                    </Button>
                 </main>
             </Container>
         </>
-    );
-  }
+    )
 }
-
-export default connect(
-  state => state,
-  actions
-)(Signin);
